9d53b56e46e760a6d633f778f3a19787a498b3c5
Alex Melman <amelman5@gmail.com>
2012-08-01 18:33:44 -0400

    Added program access key validation for signups. Added functionality to remove program access key from show program API method.

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -14,6 +14,8 @@ class ProgramsController < ApplicationController
   # GET /programs/1.json
   def show
     @program = Program.find(params[:id])
+    # Do not send the program access key to the client.
+    @program.delete(:program_access_key)
 
     respond_to do |format|
       format.html # show.html.erb
