569c81800ff16555ef1f2b01e68b15a76ec15cf9
Alex Melman <amelman5@gmail.com>
2012-09-13 18:16:15 -0400

    Made fix for worker processing.

--- app/workers/photo_import_worker.rb
+++ app/workers/photo_import_worker.rb
@@ -5,14 +5,14 @@ class PhotoImportWorker
     if Rails.env.production?
       require 'heroku-api'
       @heroku = Heroku::API.new if @heroku.nil?
-      @heroku.post_ps_scale(ENV['HEROKU_APP_NAME'], 'resque', 1)
+      @heroku.post_ps_scale(ENV['HEROKU_APP_NAME'], 'photo_importer_queue', 1)
     end
 
     program = Program.find(program_id)
     program.get_photos_by_tags
 
     if Rails.env.production?
-      @heroku.post_ps_scale(ENV['HEROKU_APP_NAME'], 'resque', 0)
+      @heroku.post_ps_scale(ENV['HEROKU_APP_NAME'], 'photo_importer_queue', 0)
     end
   end
 end
