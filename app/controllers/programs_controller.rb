c4393438dca746b6a613604a1f9d1a8ebe8185b5
Alex Melman <amelman5@gmail.com>
2012-11-09 18:07:26 -0500

    Removed app_url from Program model.

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -10,7 +10,7 @@ class ProgramsController < ApplicationController
           render json: { active: false, program_id: params[:id] }
         else
           render json: @program, only: [
-            :id, :active, :app_url, :name, :short_name, :description,
+            :id, :active, :name, :short_name, :description,
             :additional_info_1, :additional_info_2, :additional_info_3,
             :photos_updated_at, :videos_updated_at, :created_at, :updated_at
           ],
