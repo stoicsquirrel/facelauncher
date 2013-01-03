7822cc7984378e79030a71bfb794686ebfeb43cc
Alex Melman <amelman5@gmail.com>
2013-01-03 15:58:15 -0500

    Updated Rails. Added files for export mailer (WIP).

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -4,6 +4,6 @@ class ExportMailer < ActionMailer::Base
   def export_complete_email(export, user)
     @model_name = "photos"
 
-    mail(:to => user.email, :subject => "Your Facelauncher Export Has Completed")
+    mail(:to => "amelman5@gmail.com", :subject => "Your Facelauncher Export Has Completed")
   end
 end
