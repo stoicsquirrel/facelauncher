d7d1fac919b9cbbe9736727f2b7a9c8ed91f29b8
Alex Melman <amelman5@gmail.com>
2013-04-30 12:26:21 -0400

    Added back filtered files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facelauncher::Application.config.secret_token = '1667513477b9082a6764ee6db65051c4c447aeabf0baa361f29eb92307802d793288343c930b8c04d78998b24455cabdaa5490dda06329cb0938bf939e0aa75d'
